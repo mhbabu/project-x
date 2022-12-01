ca81351642ffeff28c1ec56737bf543325a06ad4
mhbabu <write2babu.cse@gmail.com>
2022-12-01 13:48:22 +0600

    Route redirection and authenticate route implementatio

---
+++ project-frontend/src/components/NotFound.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="mt-5 text-center container">
+    <div class="my-5 mx-5">
+        <h1>404</h1>
+    </div> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+  props: {
+    msg: String,
+  },
+
+  data() {
+    return {
+    };
+  },
+
+  created() {
+   
+  },
+  mounted(){
+    console.log('Not Found Page');
+  }
+
+};
+</script>
--- project-frontend/src/routes/route.js
+++ project-frontend/src/routes/route.js
@@ -4,13 +4,16 @@ import AddContact from '../components/AddContact.vue';
 import VueXComponent from '../components/VueXComponent.vue';
 import RegisterComponent from '../components/RegisterComponent.vue';
 import LoginComponent from '../components/LoginComponent.vue';
+import NotFound from '../components/NotFound.vue';
+import store from '../store';
 
 const routes = [
-    { path: '/', component: ContactList },
-    { path: '/add-contacts', component: AddContact },
-    { path: '/vue-x', component: VueXComponent },
-    { path: '/registers', component: RegisterComponent },
-    { path: '/login', component: LoginComponent },
+    { path: '/', component: ContactList, meta: { requiresAuth: false } },
+    { path: '/add-contacts', component: AddContact, meta: { requiresAuth: true } },
+    { path: '/vue-x', component: VueXComponent, meta: { requiresAuth: false } },
+    { path: '/registers', component: RegisterComponent, meta: { requiresAuth: false } },
+    { path: '/login', component: LoginComponent, meta: { requiresAuth: false } },
+    { path: '/:pathMatch(.*)*', component: NotFound, meta: { requiresAuth: false } },
   ];
 
 
@@ -19,16 +22,14 @@ const router = createRouter({
   routes
 });
 
-
-
-// router.beforeEach((to, from, next) => {
-//   if (to.matched.some(record => record.meta.requiresAuth)) {
-//       if (this.$store.state.token) {
-//           return next()
-//       }
-//       return next('login')
-//   }
-//   next();
-// });
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!store.state.token) {
+      next({ path: '/login', query: { redirect: to.fullPath } })
+    } 
+  }
+  
+  next()
+});  
 
 export default router;
